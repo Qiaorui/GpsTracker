667e729012a1cdf5a3fe438617e9b2c4c42e904e
qiaorui <qiaorui.xiang@gmail.com>
2018-02-15 17:09:52 +0100

    upgrade to api 27

--- app/src/main/java/com/sparsity/gpstracker/GpsTrackerBootReceiver.java
+++ app/src/main/java/com/sparsity/gpstracker/GpsTrackerBootReceiver.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.SystemClock;
 import android.util.Log;
 
@@ -20,8 +19,8 @@ public class GpsTrackerBootReceiver extends BroadcastReceiver {
         Intent gpsTrackerIntent = new Intent(context, GpsTrackerAlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, gpsTrackerIntent, 0);
 
-        SharedPreferences sharedPreferences = context.getSharedPreferences("com.websmithing.gpstracker.prefs", Context.MODE_PRIVATE);
-        int intervalInMinutes = sharedPreferences.getInt("intervalInMinutes", 1);
+
+        int intervalInMinutes = 1;
         Boolean currentlyTracking = true;
 
         if (currentlyTracking) {
